7c24208b6adcf24aa61e94de25354d8b44ca44d5
Markus Liechti <markus@liechti.io>
2024-01-24 11:12:57 +0100

    Add minimal dev dashboard page

--- public/index.php
+++ public/index.php
@@ -2,17 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$test = true;
-
-$request = $_SERVER;
-
-$auto = new \stdClass();
-$auto->test = "halle";
-
-for ($i = 0; $i < 100; $i++) {
-    $test = $i * 2;
-}
-
-
 xdebug_info();
 //phpinfo();
---
+++ src/Kaapiii/Auto.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kaapiii;
+
+class Auto
+{
+
+}
